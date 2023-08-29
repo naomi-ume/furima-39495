7fa7629078629d49c5f73292bc2a2108fbe65899
naomi-ume <naomi.sue.tate@gmail.com>
2023-08-29 21:03:50 +0900

    rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,27 +7,21 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
   has_many :comments
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX
   with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    # @含むこと・存在することはdeviseのデフォルト実装のため省略
+    validates :password, length: { minimum: 6 }
 
-  validates :nickname
-  validates :email,    uniqueness: true
-  # @含むこと・存在することはdeviseのデフォルト実装のため省略
-  validates :password,length: { minimum: 6 }
- 
-  
-  # 全角ひらがな、全角カタカナ、漢字
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  # 全角カタカナ
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday
+    # 全角ひらがな、全角カタカナ、漢字
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # 全角カタカナ
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
   end
-
-
-
-
 end
