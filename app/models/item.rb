54ff99a9c3e743dc95ed3f8e11b3a0adf8ae091d
naomi-ume <naomi.sue.tate@gmail.com>
2023-08-31 15:33:33 +0900

    ActiveHashのitemモデルへのアソシエーションとpriceの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,35 @@
 class Item < ApplicationRecord
-belongs_to :user
-has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+  
+  belongs_to :user
+  has_one_attached :image
 
-with_options presence: true do
-  validates :user
-  validates :item_name
-  validates :price
-  validates :description
-  validates :category_id
-  validates :item_status_id
-  validates :shipping_cost_id
-  validates :prefecture_id
-  validates :shipping_date_id
-  validates :image
-end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  with_options numericality: { other_than: 1 } do
+   validates :prefecture_id
+   validates :category_id
+   validates :item_status
+   validates :shipping_cost_id
+   validates :shipping_date_id
+  end
+  
+  with_options presence: true do
+   validates :user
+   validates :item_name
+   validates :price
+   validates :description
+   validates :category_id
+   validates :item_status_id
+   validates :shipping_cost_id
+   validates :prefecture_id
+   validates :shipping_date_id
+   validates :image
+  end
 
 end
