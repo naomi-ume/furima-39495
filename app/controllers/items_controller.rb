26ed4e6259681573622413f58c064d65e61d6312
naomi-ume <naomi.sue.tate@gmail.com>
2023-08-31 16:27:22 +0900

    itemsコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,13 +3,31 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
   def show
   end
+
+  def edit
+  end
+
 end
 
 private
 
   def item_params
     params.require(:item).permit(:item_name, :price, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
-      :shipping_date_id, :image).merge(user_id: current_user.id)
+      :shipping_date_id, :prefecture_id, :image).merge(user_id: current_user.id)
   end
