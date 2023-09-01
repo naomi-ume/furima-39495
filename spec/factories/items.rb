247584f62e778cb3a5a56f42fc858bc222140747
naomi-ume <naomi.sue.tate@gmail.com>
2023-09-01 21:49:44 +0900

    itemモデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name              { 'test' }
+    description       { 'test' }
+    category_id       { 2 }
+    item_status_id      { 2 }
+    shipping_cost_id  { 2 }
+    prefecture_id     { 2 }
+    shipping_date_id  { 2 }
+    price             { 300 }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
