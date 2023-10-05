761f78769c2b0e2647b4f89f682f90c7b65d35d3
naomi-ume <naomi.sue.tate@gmail.com>
2023-10-05 16:23:13 +0900

    テストの実施

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,7 +8,7 @@ class OrderForm
     validates :item_id
     #delivery_detailモデルのバリデーション
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality:{ other_than:0, message: "can't be blank"}
+    validates :prefecture_id, numericality:{ other_than:1, message: "Prefecture must be other than 1"}
     validates :city
     validates :block
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: 'is invalid' }
